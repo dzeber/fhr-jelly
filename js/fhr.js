fa1fc75b1f4238c6d4dd669fdbb7db73172c4ec1
David Zeber <dzeber@mozilla.com>
2013-04-12 22:56:51 -0700

    plot tweaks
    Added padding into axis scaling and point sizing, colours and date
    labels

--- js/fhr.js
+++ js/fhr.js
@@ -121,7 +121,7 @@ $(function() {
         $('.yaxis-label').show();
     }, */
     var drawGraph = function(median) {
-        if(median) {
+	    if(median) {
             drawAverageGraph();
         } else {
             drawAllGraph();
@@ -130,73 +130,132 @@ $(function() {
     
     
     drawAverageGraph = function() {
-        // Minimum height of y axis: 5 seconds. 
+        // Minimum height of y axis: 5 seconds 
         var Y_MIN_HEIGHT = 5, 
-            // Minimum number of dates on the x axis: 2 weeks. 
-            X_MIN_DAYS = Date.now() - TWO_WEEKS;
-            
-        // Padding sizes: 
+            // Minimum number of dates on the x axis: 2 weeks 
+            X_MIN_DAYS = 14, 
+			// Desired number of ticks on the y axis 
+			Y_NUM_TICKS = 5,
+			// Minimum point radius 
+			MIN_POINT_RAD = 2.5,  
+			// Maximum point radius 
+			MAX_POINT_RAD = 5, 
+			// Proportion of space allocated to a single day that the point radius should take
+			POINT_DAY_PROP = 0.5; 
+        
+    	// Padding sizes in px: 
         // Space to allocate for the axes
         var xAxisPadding = 20, 
-            yAxisPadding = 30,
-            // Padding between the left and right edges of the plot and the first and last days
-            datePadding = 10,
-            // Padding between the maximum value and the top edge of the plot
-            valuePadding = 10;
+			// Padding allocated for the y axis with ticks, but excluding labels. 
+            yAxisBasePadding = 10,
+			yAxisPadding = yAxisBasePadding,
+            // Padding to add per digit on the y-axis labels
+			perDigitPadding = 7,
+			// Vertical amount by which the top of the plot (and the y axis) should exceed the highest datapoint.
+			plotTopPadding = 20, 
+			// Padding between the left and right edges of the plot and the first and last days
+            datePadding = 20; 
+			
+			
+		// Diagnostic. 
+		var inspectData = function(dataArray) {
+			for(var i=0; i < dataArray.length; i++) { 
+				var str = "";
+				for(var k in dataArray[i]) {
+					str += k + ": " + dataArray[i][k] + ",  ";
+				}
+				console.log(str);
+			}
+		};
+		
     
         // Dimensions are determined by container. 
         var graphContainer = d3.select(".graph"), 
-            width = parseInt(graphContainer.style("width"), 10),
-            height = parseInt(graphContainer.style("height"), 10); 
+            containerWidth = parseInt(graphContainer.style("width"), 10),
+            containerHeight = parseInt(graphContainer.style("height"), 10); 
             
-        graphContainer.style("border", "1px solid black");
-           
-        var svg = graphContainer.append("svg:svg")
-            .attr("width", width + "px").attr("height", height + "px");
-        
-        // Add group for startup plot. 
+        // graphContainer.style("border", "1px solid black");
+		
+		var svg = graphContainer.append("svg:svg")
+            .attr("width", containerWidth + "px").attr("height", containerHeight + "px");
+		
+		// Retrieve data to be plotted. 
+		var graphData = getAverageGraphData();
+		// Dates are interpreted as midnight GMT. Change to midnight local time for display purposes. 
+		graphData.forEach(function(d) { d.date = d3.time.day(d.date); });
+		
+		
+		// Compute y axis padding dynamically according to the number of digits in the labels. 
+		// There is probably a better way to do this. 
+		yAxisPadding += perDigitPadding * Math.floor(d3.max(graphData, function(d) { return d.medTime; })).toString().length;
+		
+        // Add group for interior of startup plot. 
         var startup = svg.append("g")
-            .attr("transform", "translate(" + yAxisPadding + ", 0)");
+			.attr("transform", "translate(" + yAxisPadding + ", 0)");
         
         // startup.append("rect").style("stroke","blue")
             // .attr("width",width-yAxisPadding).attr("height",height-xAxisPadding);
     
-        var graphData = getAverageGraphData();
-        
-        var x = d3.time.scale()
+		var plotWidth = containerWidth - yAxisPadding, 
+			plotHeight = containerHeight - xAxisPadding;
+		
+		// Add background colour. 
+		startup.append("rect").attr("id", "startup")
+			.attr("width", plotWidth).attr("height", plotHeight);
+		
+		// Set up scale for dates on x-axis. 
+		// If earliest date is less than two weeks ago, set to two weeks ago.
+		var earliest = new Date(Math.min(d3.time.day(d3.time.day.offset(new Date(), -X_MIN_DAYS)), d3.min(graphData, function(d) { return d.date; })));
+		var x = d3.time.scale()
             // Allocate space from earliest date in the payload until today.
-            // If earliest date is less than two weeks ago, allocate space for two weeks. 
-            .domain([Math.min(X_MIN_DAYS, d3.min(graphData, function(d) { return d.date; })), Date.now()])
-            .range([0, width - yAxisPadding]);
+            .domain([earliest, d3.time.day(new Date())])
+            .range([datePadding, plotWidth - datePadding]);
+		// Update scale to add padding on the left and right. 
+		x.domain([x.invert(0), x.invert(plotWidth)]).range([0, plotWidth]);
         
+		// Set up scale for startup time values on y-axis. 
         var y = d3.scale.linear()
             // Allocate space from 0 to maximum value in dataset. 
             // If maximum value is less than Y_MIN_HEIGHT, extend to Y_MIN_HEIGHT. 
             .domain([0, Math.max(Y_MIN_HEIGHT, d3.max(graphData, function(d) { return d.medTime; }))])
-            .range([height - xAxisPadding, 0]);
-        
+            .range([plotHeight, plotTopPadding]); 
+		// Update scale to incorporate extra sapce at the top. 
+		y.domain([0, y.invert(0)]).range([plotHeight, 0]);
+			
+	
+		
         // Add axes. 
         var xAxis = d3.svg.axis()
-            .scale(x).orient("bottom");
+            .scale(x).orient("bottom")
+			.tickFormat(d3.time.format("%b %d"));
             
         var yAxis = d3.svg.axis()
             .scale(y).orient("left")
-            .ticks(5);
+            .ticks(Y_NUM_TICKS);
             
         startup.append("g")
-            .attr("class", "axis")
-            .attr("transform", "translate(0," + (height - xAxisPadding) + ")")
+            .attr("class", "x axis")
+            .attr("transform", "translate(0," + plotHeight + ")")
             .call(xAxis);
             
         startup.append("g")
-            .attr("class", "axis")
+            .attr("class", "y axis")
             .call(yAxis);
         
         // Add points. 
-        startup.selectAll(".dot")
-            .data(graphData).enter().append("circle")
+		// Remove dates with medTime undefined or null 
+		// (means that there was no appSessions.previous for that day). 
+		var startupData = graphData.filter(function(d) { return typeof d.medTime !== "undefined" && d.medTime !== null; });
+
+		// Scale the point radius according to the horizontal space allocated per day. 
+		var pointRadius = Math.min(Math.max(
+			(x(d3.time.day.offset(earliest, 1)) - x(earliest)) * POINT_DAY_PROP, 
+			MIN_POINT_RAD), MAX_POINT_RAD); 
+				
+		startup.selectAll(".dot")
+            .data(startupData).enter().append("circle")
             .attr("class", "dot")
-            .attr("r", 3.5)
+            .attr("r", pointRadius)
             .attr("cx", function(d) { return x(d.date); })
             .attr("cy", function(d) { return y(d.medTime); })
         
